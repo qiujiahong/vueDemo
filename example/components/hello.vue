549479ab8b8f834e293c0eedd12a474cead2583c
root <qiujiahongde@163.com>
2017-12-02 21:58:49 +0800

    模板

---
+++ example/components/hello.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }},我的世界</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: '欢迎！'
+    }
+  }
+}
+</script>
